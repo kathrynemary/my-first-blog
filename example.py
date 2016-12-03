555fff0cc5322e3816c0c1102aa5f852026c51ea
Kathryne Dunlap <kathrynemary@gmail.com>
2016-12-03 11:07:53 -0600

    initial commit

---
+++ example.py
@@ -0,0 +1,14 @@
+print('Hello, Kathryne!')
+
+if 3 > 2:
+    print('Yup, that\'s what I expected.')
+
+
+if "3" > "2":
+    print('How did you know that??')
+
+people = ['Helen', 'Pam', 'Wil']
+
+for name in people:
+    print("Introducing: " + name)
+    print("On to the next one...")
